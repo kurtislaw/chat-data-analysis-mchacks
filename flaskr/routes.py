007f793aced772e7fb2e9e00a34957e59dd97a54
kurtislaw <kurtislaww@gmail.com>
2022-01-22 13:08:42 -0500

    made a working zip extractor + uploader (does not redirect yet)

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -4,18 +4,28 @@ import json
 from .individual import Conversation
 from .individual import find_all_names
 import plotly
+import os
+from werkzeug.utils import secure_filename
+import zipfile
+
+UPLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def upload_file():
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        uploaded_file.save(uploaded_file.filename)
-    return redirect(url_for('index'))
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and file.filename != '':
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            zip_ref = zipfile.ZipFile(os.path.join(UPLOAD_FOLDER, filename), 'r')
+            zip_ref.extractall(UPLOAD_FOLDER)
+            zip_ref.close()
+            return filename
 
 
 @app.route('/successful')
@@ -31,4 +41,4 @@ def results():
     df = Conversation(dir)
     fig1 = df.message_over_time('flask')
     graphJSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template('test.html', graphJSON=graphJSON)
+    return render_template('results.html', graphJSON=graphJSON)
