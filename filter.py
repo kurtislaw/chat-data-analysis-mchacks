dbb44e2ee22ede0773a0dc6d43e960534eaf0182
kurtislaw <kurtislaww@gmail.com>
2022-01-22 05:14:36 -0500

    Delete filter.py

--- filter.py
+++
@@ -1,8 +0,0 @@
-"""
-Filters the messages.zip file into useful data
-"""
-from zipfile import ZipFile
-
-def extract_zip(directory: str):
-    with ZipFile(directory, 'r') as zip:
-        zip.extractall()
