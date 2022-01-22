5259f18458fd2ece629ca0a8d3e5050ceda9dbcb
kurtislaw <kurtislaww@gmail.com>
2022-01-22 03:21:14 -0500

    made dir specific

--- dataframe.py
+++ dataframe.py
@@ -2,20 +2,20 @@
 Creates a pandas dataframe containing all the information we will possibly need.
 """
 import pandas as pd
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, use
 from matplotlib.pyplot import figure
 import numpy as np
 import json
 import os
 
 
-def create_megaframe():    
+def create_megaframe(dir: str):    
     megaframe = pd.DataFrame()
-    files = os.listdir('./inbox/valerie_0p9yu22iaq')
+    files = os.listdir(dir)
 
     for file in files:
         if file.endswith(".json"):
-            with open(f'./inbox/valerie_0p9yu22iaq/{file}') as json_file:
+            with open(f'{dir}/{file}') as json_file:
                 json_data = json.load(json_file)
                 df = pd.DataFrame(json_data['messages'])
                 megaframe = pd.concat([megaframe, df])
@@ -24,6 +24,9 @@ def create_megaframe():
     megaframe['timestamp_ms'] = pd.to_datetime(megaframe['timestamp_ms'], unit='ms')
     
     # dropping unnecessary data
-    megaframe.drop(['share', 'photos', 'reactions', 'videos', 'audio_files'], axis=1, inplace=True)
-    
+    useless_columns = ['share', 'photos', 'reactions', 'videos', 'audio_files']
+    for column in useless_columns:
+        if column in megaframe.columns:
+            megaframe.drop(column, axis=1, inplace=True)
+
     return megaframe
