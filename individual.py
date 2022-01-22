1b98e2669395663a834a57e8deeacf780a46c0f7
kurtislaw <kurtislaww@gmail.com>
2022-01-22 05:05:08 -0500

    recreated all of Shaan's functions but with dataframe instead hehehehe

--- individual.py
+++ individual.py
@@ -3,19 +3,30 @@ Individuals
 """
 import os
 from typing import Tuple
+import dataframe
 
 
-# 1. Selector
-# 2. Analysis
+class CONVERSATION:
+    def __init__(self, df) -> None:
+        self.df = dataframe.create_megaframe(df)
 
-# def lists_all_friends() -> tuple:
-#     """Returns a tuple containing all friends in the messages folder."""
-#     rootdir = '/messages/'
+    def individual_messages_count(self) -> dict:
+        """Return a dict where the keys are people and values are message count."""
+        return dict(self.df['sender_name'].value_counts())
 
+    def people(self) -> tuple:
+        """Returns a tuple containing the people involved in this conversation."""
+        return tuple(self.df['sender_name'].unique())
 
-def list_all_directories() -> Tuple[str]:
-    """Returns all the directories as tuples containing strings"""
-    root_dir = "./messages/"
-    for subdir, dirs, files in os.walk(root_dir):
-        for file in files:
-            print(os.path.join(subdir, file))
+    def total_message_count(self) -> int:
+        """Returns total messages sent in the conversation as an int"""
+        return self.df['sender_name'].count()
+
+    def common_words(self):
+        """Returns a dict with the most common words"""
+        sentences = list(self.df['content'])
+        words = []
+        for sentence in sentences:
+            for word in sentence.split():
+                words.append(word)
+        return words
